38369a0ca9189dfb1896a922f56bc91a6eb535de
OlojaMuyiwa <olojam4969@gmail.com>
2023-01-30 14:02:10 +0100

    refactored

--- test/main.c
+++ test/main.c
@@ -1,8 +1,14 @@
 #include "../main.h"
 #include <stdio.h>
+
 int main(void)
 {
-	_printf("hello %c %% %s world\n", 'h', "please");
-	printf("hello %c %% %s world\n", 'h', "please");
+	_printf("hello %c %% %s  world\n", 'h', "please");
+	// printf("hello %c %% %s world\n", 'h', "please");
+
+	// testing integer
+	_printf("%d\n", 2060);
+	// printf("%d\n", 2060);
+
 	return (0);
 }
